51082cdd1a42fd8440060e77de0a604a43b854d3
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-10-03 20:43:54 -0230

    react practice :D

--- react-practice/react-app/src/App.tsx
+++ react-practice/react-app/src/App.tsx
@@ -1,7 +1,35 @@
-import Message from "./message";
-
-function App() {
-  return<div><Message /></div>
+function MyHeading() {
+  return (
+    <nav className="nav_heading">
+    <h1>This is a heading</h1>
+    <div className="nav_items">
+    <ul>item1</ul>
+    <ul>item2</ul>
+    <ul>item3</ul>
+    <ul>item4</ul>
+    </div>
+    </nav>
+  );
 }
 
-export default App;
+function MyBody() {
+  return(
+    <body>
+      <div className="body_class">
+        <button className="button_class">
+          This is a button
+        </button>
+      </div>
+    </body>
+  );
+}
+
+
+export default function MyApp() {
+  return (
+    <>
+      <MyHeading />
+      <MyBody />
+    </>
+  );
+}
--- react-practice/react-app/src/message.tsx
+++
@@ -1,9 +0,0 @@
-
-function Message() {
-    const name = "World";
-    if (name)
-        return <h1>Hello {name}</h1>;
-    return <h1>Hello Wordl</h1>
-}
-
-export default Message;
