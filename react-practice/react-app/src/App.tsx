49ba33bc63b8a449d9a4aaccec19d0106369a6c9
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-10-04 22:05:42 -0230

    i understand react

--- react-practice/react-app/src/App.tsx
+++ react-practice/react-app/src/App.tsx
@@ -19,10 +19,26 @@ function MyBody() {
       <div className="body_class">
         <button className="button_class">This is a button</button>
         <p>this ia a sentence</p>
+        <h2>{user.name}</h2>
+        <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
       </div>
     </main>
   );
 }
+const user = {
+  name: 'ammar',
+  imageUrl: 'https://avatars.githubusercontent.com/u/71677758?v=4',
+  imageSize: 90,
+};
+
 export default function MyApp() {
   return (
     <>
